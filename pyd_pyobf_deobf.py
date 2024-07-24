30af52feae06f97d6961829b51da2e622f3d402e
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2024-07-24 10:55:46 +0700

    fix

--- pyd_pyobf_deobf.py
+++ pyd_pyobf_deobf.py
@@ -4,17 +4,14 @@
 
 class compile:
     def __init__(self):
-        self.com = vars(__import__('builtins')).copy()['compile']
-
+        return
     def __call__(self, *args, **kwargs):
-        p = __import__('ast').unparse(args[0])
-        print(eval(p[4:]))
+        print(eval(__import__('ast').unparse(args[0])[4:]))
         exit(0)
 
 compile = compile()
 __import__('builtins').compile = compile
 
 file = open(input(">> input file path: "), 'rb').read()
-
 print('>> source code: ')
 exec(file)
